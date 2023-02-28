ea3f226dee091361007d1aac5310abb81e0b81fe
Drissa <dcompte223@gmail.com>
2023-02-28 13:55:22 +0100

    menu js add

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ $(document).ready(function () {
     );
   });
 });
+
+// menu content display
+
+const hamburgerToggler = document.querySelector('.hamburger');
+const log = document.querySelector('.logo');
+const navLinksContainer = document.querySelector('.nav_links');
+const clo = document.querySelectorAll('.scroll');
+const toggleNav = () => {
+  hamburgerToggler.classList.toggle('open');
+  const ariaToggle =
+    hamburgerToggler.getAttribute('aria-expanded') === 'true'
+      ? 'false'
+      : 'true';
+  hamburgerToggler.setAttribute('aria-expanded', ariaToggle);
+  navLinksContainer.classList.toggle('open');
+  if (ariaToggle === 'true') {
+    log.style.visibility = 'hidden';
+    clo.forEach((element) => {
+      element.addEventListener('click', function () {
+        navLinksContainer.classList.remove('open');
+        hamburgerToggler.classList.remove('open');
+        hamburgerToggler.setAttribute('aria-expanded', 'false');
+        log.style.visibility = 'visible';
+      });
+    });
+  } else {
+    log.style.visibility = 'visible';
+  }
+};
+hamburgerToggler.addEventListener('click', toggleNav);
